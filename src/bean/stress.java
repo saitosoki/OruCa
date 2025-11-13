ef9a364535cda2f597ce7c549964b74505c27ba7
Nobuki <ymg2465014@stu.o-hara.ac.jp>
2025-11-13 10:54:06 +0900

    stress

---
+++ src/bean/stress.java
@@ -0,0 +1,36 @@
+package bean;
+
+import java.sql.Date;
+
+public class stress implements java.io.Serializable {
+
+	private int user_num;             // 利用者番号
+	private int stress_score_num;     // ストレススコア
+	private Date date;                // 日付
+
+	public stress() {}
+
+	public int getUser_num() {
+		return user_num;
+	}
+
+	public int getStress_score_num() {
+		return stress_score_num;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setUser_num(int user_num) {
+		this.user_num = user_num;
+	}
+
+	public void setStress_score_num(int stress_score_num) {
+		this.stress_score_num = stress_score_num;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+}
