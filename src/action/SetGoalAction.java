f9c5092f9dce28ad39c6969d6a64dc17c971cb9a
taku <ymg2465044@stu.o-hara.ac.jp>
2025-11-13 14:41:29 +0900

    abgb

--- src/action/SetGoalAction.java
+++ src/action/SetGoalAction.java
@@ -1,5 +1,28 @@
 package action;
 
-public class SetGoalAction {
+import java.io.IOException;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/setGoal")
+public class SetGoalAction extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        request.setCharacterEncoding("UTF-8");
+        String goal = request.getParameter("goal");
+
+        // 簡単な保存例（今回はセッションに保存するだけ）
+        HttpSession session = request.getSession();
+        session.setAttribute("todayGoal", goal);
+
+        // goal設定後にスケジュール一覧画面へ移動
+        response.sendRedirect("scheduleList.jsp");
+    }
 }
