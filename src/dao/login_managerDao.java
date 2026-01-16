4e6e9fa2c2ae0ffe127f813fba0bdecb249ccba4
ono504 <ymg2465047@stu.o-hara.ac.jp>
2026-01-16 11:54:35 +0900

    daos

---
+++ src/dao/login_managerDao.java
@@ -0,0 +1,92 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import action.LoginAction;
+
+public class login_managerDao {
+
+	private static final String DB_URL ="jdbc:h2:tcp://localhost/~/oruca";
+	private static final String DB_USER = "sa";
+	private static final String DB_PASS = "";
+	private static final String DRIVER_NAME = "org.h2.Driver";
+
+    private Connection getConnection() throws SQLException {
+        try {
+            Class.forName(DRIVER_NAME);
+
+            return DriverManager.getConnection(DB_URL, DB_USER, DB_PASS);
+        } catch (ClassNotFoundException e) {
+
+            throw new SQLException("JDBCドライバが見つかりません。", e);
+        }
+    }
+
+    // --- ログイン検索メソッド ---
+    /**
+     * 指定されたログイン名とパスワードを持つユーザーをデータベースから検索する。
+     * @param login ユーザーが入力したログインID
+     * @param password ユーザーが入力したパスワード
+     * @return ユーザーが存在すればLoginActionオブジェクト、存在しなければnull
+     * @throws Exception SQL関連のエラーが発生した場合
+     */
+    public LoginAction search(String login, String password) throws Exception {
+
+        LoginAction customer = null;
+        Connection con = null;
+        PreparedStatement st = null;
+        ResultSet rs = null;
+
+        try {
+
+        	int userNum = 0;
+            try {
+                userNum = Integer.parseInt(login);
+            } catch (NumberFormatException e) {
+                return null;
+            }
+
+            // データベース接続を取得
+
+            con = getConnection();
+            st = con.prepareStatement(
+
+                "select * from USER where USER_NUM=? and PASSWORD=?"
+            );
+
+            st.setInt(1, userNum);
+            st.setString(2, password);
+
+            rs = st.executeQuery();
+
+            if (rs.next()) {
+                customer = new LoginAction();
+
+                customer.setId(rs.getInt("USER_NUM"));
+                customer.setLogin(rs.getString("EMAIL"));
+                customer.setPassword(rs.getString("PASSWORD"));
+            }
+
+            return customer;
+
+        } catch (Exception e) {
+        	e.printStackTrace();
+
+            throw e;
+        } finally {
+
+            try {
+                if (rs != null) rs.close();
+                if (st != null) st.close();
+                if (con != null) con.close();
+            } catch (SQLException closeE) {
+
+                closeE.printStackTrace();
+            }
+        }
+    }
+}
